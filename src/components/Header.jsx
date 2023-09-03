50251cbb6cb2699c679f8c96b84ed5e3944781c6
Alexander Bird <alex@alexanderbird.software>
2023-09-02 19:58:27 -0700

    Use Material UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,27 @@
 import { useLocation } from 'preact-iso';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export function Header() {
-	const { url } = useLocation();
+  const { url } = useLocation();
 
-	return (
-		<header>
-			<nav>
-				<a href="/" class={url == '/' && 'active'}>
-					Home
-				</a>
-				<a href="/404" class={url == '/404' && 'active'}>
-					404
-				</a>
-			</nav>
-		</header>
-	);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            🧈 Butter
+          </Typography>
+          <Button color="inherit" href="/" variant={url === '/' ? 'contained' : ''}>Home</Button>
+          <Button color="inherit" href="/404" variant={url === '/404' ? 'contained' : ''}>404</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
+
