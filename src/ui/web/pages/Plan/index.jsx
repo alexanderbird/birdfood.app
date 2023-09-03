dc574c2b7e4c6de052edfb24b8f3a93a2e020520
Alexander Bird <alex@alexanderbird.software>
2023-09-03 15:43:12 -0700

    When an item is selected on the Plan page, add it to the list

--- src/ui/web/pages/Plan/index.jsx
+++ src/ui/web/pages/Plan/index.jsx
@@ -1,21 +1,35 @@
+import { useState, useEffect } from 'preact/hooks';
+import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
 import { Header } from '../../components/Header.jsx';
 import { StaticData } from '../../../../data/static';
 import { Core } from '../../../../core';
 
 export function Plan() {
+  const [items, setItems] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const addItem = item => setSelectedItems(items => [item, ...items]);
   const core = new Core(new StaticData());
+  useEffect(() => {
+    setItems(core.listItems()); 
+  }, []);
   return (<>
     <Header>
       Plan the next shop
     </Header>
-    <Container maxWidth="sm">
-      <ComboBox items={asItems(core.listItems())} />
-      Coming soon
-    </Container>
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <Container maxWidth="sm">
+        <ComboBox items={asItems(items)} onSelect={addItem}/>
+        Coming soon
+      </Container>
+      <TheList items={selectedItems} />
+    </Box>
   </>);
 }
 
@@ -26,9 +40,37 @@ function asItems(groceryItems) {
   }));
 }
 
+const TheList = ({ items }) => {
 
-function ComboBox({ items }) { return ( <Autocomplete disablePortal
+  return (
+    <List>{ items.map(item => 
+      <ListItem disablePadding>
+        <ListItemText primary={item.label} />
+      </ListItem>
+    ) }</List>
+  );
+}
+
+
+function ComboBox({ items, onSelect }) {
+  const [value, setValue] = useState(" ");
+  return (
+    <Autocomplete disablePortal
+      handleHomeEndKeys
+      autoComplete
+      autoHighlight
+      blurOnSelect
+      freeSolo
+      clearOnBlur
+      includeInputInList
+      inputValue={value}
       options={items}
+      onChange={(event, item) => {
+        setValue(" ");
+        if (item) {
+          onSelect(item);
+        }
+      }}
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="Add Item" />}
     />
