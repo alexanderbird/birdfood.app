6fdabdbe4cb64b3295fd5cfba2d5f400828e2dd5
Alexander Bird <alex@alexanderbird.software>
2023-09-04 13:07:22 -0700

    Add missing frozen food icon

--- src/ui/web/components/ItemTypeIcon/index.js
+++ src/ui/web/components/ItemTypeIcon/index.js
@@ -3,6 +3,7 @@ import { BakeryIcon } from './BakeryIcon';
 import { DeliIcon } from './DeliIcon';
 import { DairyIcon } from './DairyIcon';
 import { BabyIcon } from './BabyIcon';
+import { FrozenFoodIcon } from './FrozenFoodIcon';
 
 export const ItemTypeIcon = ({ type }) => {
   if (type === "DRY_GOOD") return <DryGoodIcon />
@@ -10,5 +11,6 @@ export const ItemTypeIcon = ({ type }) => {
   if (type === "DELI") return <DeliIcon />
   if (type === "DAIRY") return <DairyIcon />
   if (type === "BABY") return <BabyIcon />
+  if (type === "FROZEN") return <FrozenFoodIcon />
   return null;
 }
