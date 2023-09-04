5fd9a135155796c864db3370ad4e8214682802bf
Alexander Bird <alex@alexanderbird.software>
2023-09-04 10:09:49 -0700

    Add missing item type icons

--- src/ui/web/components/ItemTypeIcon/index.js
+++ src/ui/web/components/ItemTypeIcon/index.js
@@ -1,10 +1,14 @@
 import { DryGoodIcon } from './DryGoodIcon';
 import { BakeryIcon } from './BakeryIcon';
 import { DeliIcon } from './DeliIcon';
+import { DairyIcon } from './DairyIcon';
+import { BabyIcon } from './BabyIcon';
 
 export const ItemTypeIcon = ({ type }) => {
   if (type === "DRY_GOOD") return <DryGoodIcon />
   if (type === "BAKERY") return <BakeryIcon />
   if (type === "DELI") return <DeliIcon />
+  if (type === "DAIRY") return <DairyIcon />
+  if (type === "BABY") return <BabyIcon />
   return null;
 }
