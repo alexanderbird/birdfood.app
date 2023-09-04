fd49e287989491666309ccfb196274a976b3f4ba
Alexander Bird <alex@alexanderbird.software>
2023-09-03 23:25:18 -0700

    Add a dropdown for changing the quantity in the Plan tab

--- src/ui/web/pages/Plan/index.jsx
+++ src/ui/web/pages/Plan/index.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'preact/hooks';
 import Autocomplete from '@mui/material/Autocomplete';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import Box from '@mui/material/Box';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import Container from '@mui/material/Container';
@@ -38,15 +43,17 @@ export function Plan() {
     triggerUpdate();
   }
 
+  const setQuantity = (id, quantity) => {
+    core.setItemPlannedQuantity(id, quantity);
+    triggerUpdate();
+  }
+
   const clearAll = () => {
     core.removeItemsFromShoppingList(selectedItems.map(x => x.value.Id));
     triggerUpdate();
   }
 
-  const addItem = item => {
-    core.setItemPlannedQuantity(item.value.Id, 1);
-    triggerUpdate();
-  }
+  const addItem = item => setQuantity(item.value.Id, 1);
 
   useEffect(() => {
     const { shoppingList, unselectedItems } = core.getShoppingList();
@@ -61,7 +68,7 @@ export function Plan() {
     <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <Container maxWidth="sm">
         <ComboBox items={items} onSelect={addItem}/>
-        <TheList items={selectedItems} removeAll={openConfirmEmptyDialog} updateQuantity={updateQuantity} />
+        <TheList items={selectedItems} removeAll={openConfirmEmptyDialog} updateQuantity={updateQuantity} setQuantity={setQuantity} />
       </Container>
     </Box>
     <Dialog open={confirmEmptyDialogOpen} onClose={closeConfirmEmptyDialog}>
@@ -91,7 +98,12 @@ function asItems(groceryItems) {
   }));
 }
 
-const TheList = ({ items, removeAll, updateQuantity }) => {
+const TheList = ({ items, removeAll, updateQuantity, setQuantity }) => {
+
+  const MinusIcon = ({ item }) =>
+    item.value.PlannedQuantity > 1
+    ? <RemoveIcon sx={{ fontSize: 14 }} />
+    : <DeleteForeverIcon sx={{ fontSize: 14 }} />;
 
   return (
     <List>{ items.map(item => 
@@ -99,9 +111,9 @@ const TheList = ({ items, removeAll, updateQuantity }) => {
       <ListItem sx={{ flexDirection: 'column' }}>
         <ListItemText primary={item.label} sx={{ alignSelf: 'flex-start' }}/>
         <ButtonGroup sx={{ width: '100%', justifyContent: 'flex-end' }}>
-          <Button onClick={() => updateQuantity(item.value.Id, -1)}><RemoveIcon sx={{ fontSize: 10 }}/></Button>
-          <Button><Typography sx={{ fontSize: 10 }}>{item.value.PlannedQuantity}</Typography></Button>
-          <Button onClick={() => updateQuantity(item.value.Id, 1)}><AddIcon sx={{ fontSize: 10 }}/></Button>
+          <Button onClick={() => updateQuantity(item.value.Id, -1)}><MinusIcon item={item} /></Button>
+          <QuantitySelector value={item.value.PlannedQuantity} onChange={quantity => setQuantity(item.value.Id, quantity)} />
+          <Button onClick={() => updateQuantity(item.value.Id, 1)}><AddIcon sx={{ fontSize: 14 }}/></Button>
         </ButtonGroup>
       </ListItem>
       <Divider component="li" />
@@ -113,6 +125,14 @@ const TheList = ({ items, removeAll, updateQuantity }) => {
       </ListItem>
     }
     </List>
+  );
+}
+
+function QuantitySelector({ value, onChange }) {
+  return (
+    <Select value={value} onChange={(event, object) => onChange(object.props.value)} sx={{ borderRadius: 0, height: 28 }}>
+      { Array.from(Array(10).keys()).map(i => <MenuItem value={i}>{i}</MenuItem>) }
+    </Select>
   );
 }
 
