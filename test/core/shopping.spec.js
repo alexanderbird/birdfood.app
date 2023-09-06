9481194312a0f98e59ec97d6e6f68ed367194f9a
Alexander Bird <alex@alexanderbird.software>
2023-09-05 19:16:53 -0700

    Write out testing todo list for shopping APIs

--- test/core/shopping.spec.js
+++ test/core/shopping.spec.js
@@ -5,8 +5,8 @@ import { describe, it, expect } from 'vitest';
 describe('core shopping APIs', () => {
   it('generates a reasonable ID when you start shopping', () => {
     const core = new Core(new StaticData());
-    const shopping = core.startShopping();
-    expect(shopping.Id).toMatch(/^s-[a-z0-9]{12}$/);
+    const shoppingEvent = core.startShopping();
+    expect(shoppingEvent.Id).toMatch(/^s-[a-z0-9]{12}$/);
   });
 
   it.skip('generates a new ID each time you start shopping', () => {
@@ -16,4 +16,11 @@ describe('core shopping APIs', () => {
       .map(x => x.Id);
     expect(new Set(allIds).size).toEqual(soMany);
   });
+
+  // can complete items for a shopping event
+  // does not allow completing items for non-existent shopping events
+  // can list shopping events with status
+  // can complete a shopping event
+  // cannot complete items for a complete shopping event
+  // can get a shopping event list which includes the merge of all items in the shopping list and all the completed events in the shopping event
 });
