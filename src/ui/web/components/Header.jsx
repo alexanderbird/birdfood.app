165bead9c1d7bec57d8cb40de2c2d7295d7adca8
Alexander Bird <alex@alexanderbird.software>
2023-09-08 16:21:19 -0700

    Update the Header to be fixed to the top and to hide on scroll

--- src/ui/web/components/Header.jsx
+++ src/ui/web/components/Header.jsx
@@ -1,14 +1,27 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import Slide from '@mui/material/Slide';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 
 export function Header({ children }) {
   return (
     <Box sx={{ flexGrow: 1, pb: 2 }}>
-      <AppBar position="static">
-        <Toolbar>{ children }</Toolbar>
-      </AppBar>
+      <HideOnScroll>
+        <AppBar>
+          <Toolbar>{ children }</Toolbar>
+        </AppBar>
+      </HideOnScroll>
     </Box>
   );
 }
 
+const HideOnScroll = ({ children }) => {
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+};
