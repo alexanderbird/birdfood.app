84bcb9b075e78b1b88fa51c3f38347615edcd3ca
Alexander Bird <alex@alexanderbird.software>
2023-09-08 15:05:45 -0700

    Update the currency input to use decimal keyboard

--- src/ui/web/components/CurrencyTextField.jsx
+++ src/ui/web/components/CurrencyTextField.jsx
@@ -35,7 +35,7 @@ export const CurrencyTextField = ({ setValue, ...props }) => {
       inputProps={{
         sx: { textAlign: 'right' },
         type: 'number',
-        inputMode: 'numeric'
+        inputMode: 'decimal'
       }}
       InputProps={{
         startAdornment: <InputAdornment position="start">$</InputAdornment>,
