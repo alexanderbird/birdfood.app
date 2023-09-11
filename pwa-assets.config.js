6166a8d5a54aa149aa746841728e961163eab3f4
Alexander Bird <alex@alexanderbird.software>
2023-09-11 11:49:29 -0700

    Update Vite PWA config to get the icon assets to work on iOS

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -3,12 +3,6 @@ import { defineConfig, minimalPreset as preset } from '@vite-pwa/assets-generato
 export default defineConfig({
   preset,
   images: [
-    'public/favicon.svg'
-    'public/apple-touch-icon-180x180.pngg',
-    'public/pwa-64x64.pngg',
-    'public/pwa-192x192.pngg',
-    'public/favicon.icog',
-    'public/pwa-512x512.pngg',
-    'public/maskable-icon-512x512.pngg',
+    'public/favicon.svg',
   ]
 })
