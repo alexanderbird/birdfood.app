b7961b6872f7d7a575b9fe6f94005ca022b071e1
Alexander Bird <alex@alexanderbird.software>
2023-09-11 21:33:43 -0700

    Update the page loading spinner to be centered vertically

--- src/ui/web/components/Spinner.jsx
+++ src/ui/web/components/Spinner.jsx
@@ -2,6 +2,6 @@ import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 
 export const Spinner = () => 
-  <Box display="flex" justifyContent="space-around">
+  <Box display="flex" justifyContent="space-around" flexDirection="column" alignItems="center" sx={{ minHeight: '80vh' }}>
     <CircularProgress />
   </Box>;
