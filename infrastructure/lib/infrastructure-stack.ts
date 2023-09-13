42ad9140966de758d00c878b7f8b60685275173b
Alexander Bird <alex@alexanderbird.software>
2023-09-13 16:52:38 -0700

    Add basic dyanamodb table for persisting the shopping list

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new dynamodb.Table(this, 'BirdFoodItems', {
+      tableName: 'BirdFoodItems',
+      partitionKey: {
+        name: 'Household',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'Id',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
   }
 }
