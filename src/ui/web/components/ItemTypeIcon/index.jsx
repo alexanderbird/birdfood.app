597b54d23c879ef1afb093b6637598a0bec99d1e
Alexander Bird <alex@alexanderbird.software>
2023-09-23 18:53:05 -0700

    Add two more item types: specialty item and pharmacy item

--- src/ui/web/components/ItemTypeIcon/index.jsx
+++ src/ui/web/components/ItemTypeIcon/index.jsx
@@ -5,6 +5,8 @@ import { DairyIcon } from './DairyIcon';
 import { BabyIcon } from './BabyIcon';
 import { ProduceIcon } from './ProduceIcon';
 import { FrozenFoodIcon } from './FrozenFoodIcon';
+import { SpecialtyItemIcon } from './SpecialtyItemIcon';
+import { PharmacyIcon } from './PharmacyIcon';
 import { OtherFoodIcon } from './OtherFoodIcon';
 
 export const ItemTypeIcon = ({ type, ...props }) => {
@@ -15,17 +17,21 @@ export const ItemTypeIcon = ({ type, ...props }) => {
   if (type === "BABY") return <BabyIcon {...props} />;
   if (type === "PRODUCE") return <ProduceIcon {...props} />;
   if (type === "FROZEN") return <FrozenFoodIcon {...props} />;
+  if (type === "PHARMACY") return <PharmacyIcon {...props} />;
+  if (type === "SPECIALTY") return <SpecialtyItemIcon {...props} />;
   if (type === "OTHER") return <OtherFoodIcon {...props} />;
   return null;
 };
 
 export const ItemType = {
-  BABY: { key: "BABY", label: "Baby Food" },
+  BABY: { key: "BABY", label: "Baby Product" },
   DRY_GOOD: { key: "DRY_GOOD", label: "Dry Goods" },
   DAIRY: { key: "DAIRY", label: "Dairy" },
   DELI: { key: "DELI", label: "Deli" },
   BAKERY: { key: "BAKERY", label: "Bakery" },
   PRODUCE: { key: "PRODUCE", label: "Produce" },
   FROZEN: { key: "FROZEN", label: "Frozen" },
+  SPECIALTY: { key: "SPECIALTY", label: "Specialty Item" },
+  PHARMACY: { key: "PHARMACY", label: "Pharmacy" },
   OTHER: { key: "OTHER", label: "Other" }
 };
