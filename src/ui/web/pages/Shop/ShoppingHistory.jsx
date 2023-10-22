abe2d1674675be9031fd99cca4c4ee4930e2785f
Alexander Bird <alex@alexanderbird.software>
2023-10-21 22:09:04 -0700

    Update the shopping history page to add loading indicator and week subtotals

--- src/ui/web/pages/Shop/ShoppingHistory.jsx
+++ src/ui/web/pages/Shop/ShoppingHistory.jsx
@@ -1,13 +1,16 @@
+import { Fragment } from 'preact';
 import { useLocation } from 'preact-iso';
 
 import Container from '@mui/material/Container';
 import Alert from '@mui/material/Alert';
+import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import * as colors from '@mui/material/colors';
 
 import { Currency } from '../../components/Currency';
 import { useRecentShoppingEvents } from './useRecentShoppingEvents';
@@ -15,21 +18,32 @@ import { Page } from '../../components/Page';
 import { ShoppingHistoryPageHeader } from './ShoppingHistoryPageHeader';
 import { ShoppingEventSummary } from './ShoppingEventSummary';
 
+function getWeekOfMonth(date) {
+  const dateOfMonth = date.getDate();
+  return Math.floor((dateOfMonth - 1) / 7);
+}
+
 function useRecentShoppingEventsGroupedMonthly(core) {
   const [shoppingEvents] = useRecentShoppingEvents(core);
-  if (!shoppingEvents) return [];
+  if (!shoppingEvents) return false;
   const groupedShoppingEvents = shoppingEvents.reverse().reduce((grouped, event) => {
     const date = new Date(Date.parse(event.StartedAt));
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const group = `${year}-${month}`;
-    grouped[group] = grouped[group] || { group, date, total: 0, events: [] };
-    grouped[group].events.push(event);
-    grouped[group].total += (Number(event.TotalSpent) || 0);
+    const weekOfMonth = getWeekOfMonth(date);
+    grouped[group] = grouped[group] || { group, date, total: 0, events: [], weeks: [] };
+    grouped[group].events.push({ ...event, parsedDate: date });
+    grouped[group].weeks[weekOfMonth] = grouped[group].weeks[weekOfMonth] || { weekOfMonth, total: 0 };
+    const totalSpentNumber = (Number(event.TotalSpent) || 0);
+    grouped[group].weeks[weekOfMonth].total += totalSpentNumber;
+    grouped[group].total += totalSpentNumber;
     return grouped;
   }, {});
   return Object.values(groupedShoppingEvents).sort((lhs, rhs) => lhs.date < rhs.date ? 1 : -1);
 }
+
+const weekNumberWord = weekNumber => ['One', 'Two', 'Three', 'Four', 'Five'][weekNumber];
 
 export const ShoppingHistory = ({ core }) => {
   const location = useLocation();
@@ -52,23 +66,34 @@ export const ShoppingHistory = ({ core }) => {
                   <Typography variant="inherit" sx={{ fontWeight: 'bold' }}> <Currency>{group.total}</Currency></Typography>
                 </Typography>
                 <List dense>
-                  { group.events.sort((lhs, rhs) => lhs.StartedAt < rhs.StartedAt ? 1 : -1).map(shoppingEvent => (
-                    <ListItem
-                      disableGutters
-                      sx={{ mx: -2, px: 2, width: 'auto' }}
-                      key={shoppingEvent.Id}
-                      selected={shoppingEvent.Status === "IN_PROGRESS" || shoppingEvent.Id === activeEvent}
-                      onClick={() => location.route(`/history/${shoppingEvent.Id}`)}
-                    >
-                      <ListItemText primary={<ShoppingEventSummary variant="primary" event={shoppingEvent} />} />
-                      <Box sx={{textAlign: 'right'}}>
-                        { shoppingEvent?.Status === "IN_PROGRESS"
-                          ? <Typography>In Progress</Typography>
-                          : <Currency>{shoppingEvent.TotalSpent}</Currency>
-                        }
-                      </Box>
-                    </ListItem>
-                  )) }
+                  { group.events.sort((lhs, rhs) => lhs.StartedAt < rhs.StartedAt ? 1 : -1)
+                    .map((shoppingEvent, i, sortedShoppingEvents) => (<Fragment key={shoppingEvent.Id}>
+                      { (i === 0 || getWeekOfMonth(shoppingEvent.parsedDate) !== getWeekOfMonth(sortedShoppingEvents[i - 1].parsedDate))
+                        ? [group.weeks[getWeekOfMonth(shoppingEvent.parsedDate)]].map(week => (
+                          <Divider key={week.weekOfMonth} sx={{ color: colors.grey[500], mt: 1.5 }} textAlign="left">
+                            <Box component="span">
+                              <Box component="span">Week {weekNumberWord(week.weekOfMonth)}: </Box>
+                              <Box component="span" sx={{ fontWeight: 'bold' }}><Currency >{week.total}</Currency></Box>
+                            </Box>
+                          </Divider>
+                        ))
+                        : null
+                      }
+                      <ListItem
+                        disableGutters
+                        sx={{ mx: -2, px: 2, width: 'auto' }}
+                        selected={shoppingEvent.Status === "IN_PROGRESS" || shoppingEvent.Id === activeEvent}
+                        onClick={() => location.route(`/history/${shoppingEvent.Id}`)}
+                      >
+                        <ListItemText primary={<ShoppingEventSummary variant="primary" event={shoppingEvent} />} />
+                        <Box sx={{textAlign: 'right'}}>
+                          { shoppingEvent?.Status === "IN_PROGRESS"
+                            ? <Typography>In Progress</Typography>
+                            : <Currency>{shoppingEvent.TotalSpent}</Currency>
+                          }
+                        </Box>
+                      </ListItem>
+                    </Fragment>)) }
                 </List>
               </Box></ListItem>
             )) }
